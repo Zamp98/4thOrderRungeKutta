82980f3fc666199e66cdcc610f1fcbca57f24a12
Zamp98 <117121611+Zamp98@users.noreply.github.com>
2023-05-08 23:02:26 -0300

    correção

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ for i in range(n):
     k1 = function(y, t, h)
     k2 = function(y+(k1/2),t+(h/2), h)
     k3 = function(y+(k2/2), t+(h/2), h)
-    k4 = function(y+(k3/2), t+h, h)
+    t += h
+    k4 = function(y+(k3/2), t, h)
     y = y + (k1+2*k2+2*k3+k4)/6
     print("Iteration ",i,":",k1,k2,k3,k4,y)
